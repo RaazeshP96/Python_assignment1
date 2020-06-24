e13dc32c68b03d1629ea29091b6f82744098b015
Raazeshp96 <raazeshp96@gmail.com>
2020-06-25 04:44:42 +0545

    solution of qn 46

---
+++ dataTypes46.py
@@ -0,0 +1,12 @@
+'''
+Write a Python program to find the length of a tuple 
+'''
+
+
+def lenTup(tup):
+    return f"Length of given tuple is {len(tup)}"
+
+
+# sample tuple
+tup = ('a', 'b', 'c', 'd')
+print(lenTup(tup))
