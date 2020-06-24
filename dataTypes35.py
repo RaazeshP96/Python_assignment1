cd4e567b3a6f8ffeb837d4ef3e242a12bb5f4e35
Raazeshp96 <raazeshp96@gmail.com>
2020-06-25 04:42:11 +0545

    solution of qn 35

---
+++ dataTypes35.py
@@ -0,0 +1,11 @@
+'''
+
+Write a Python program to iterate over dictionaries using for loops.
+
+'''
+# sample value
+dic = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+
+# iterate over dictionaries using for loops.
+for key, value in dic.items():
+    print(f'{key} -> {value}')
