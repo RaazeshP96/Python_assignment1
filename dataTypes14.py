34133250d1ef1348b7097a5ae1d330182ba6b324
Raazeshp96 <raazeshp96@gmail.com>
2020-06-24 21:26:49 +0545

    solution of qn 14

---
+++ dataTypes14.py
@@ -0,0 +1,17 @@
+'''
+Write a Python function to create the HTML string with tags around the word(s).
+
+Sample function and result:
+    add_tags('i', 'Python') -> '<i>Python</i>'
+    add_tags('b', 'Python Tutorial') -> '<b>Python Tutorial </b>'
+
+'''
+
+
+def add_tags(tag, word):
+    return f" <{tag}>{word}</{tag}> "
+
+
+tag = input("enter the tag:")
+word = input("enter the word:")
+print(add_tags(tag, word))
