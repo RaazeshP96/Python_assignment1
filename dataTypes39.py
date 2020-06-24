f3eec6d5fae0b02c92c80ee0684ab3a0a4c4b57a
Raazeshp96 <raazeshp96@gmail.com>
2020-06-25 04:42:46 +0545

    solution of qn 39

---
+++ dataTypes39.py
@@ -0,0 +1,14 @@
+'''
+
+Write a Python program to unpack a tuple in several variables.
+
+'''
+# let given tuples
+tup = (1, 2, 3, 4)
+
+# unpacking tuples
+(a, b, c, d) = tup
+print(a)
+print(b)
+print(c)
+print(d)
