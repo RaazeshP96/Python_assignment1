29f06ea12513a6c28d2beb4a3e8303cb6d211cfc
Raazeshp96 <raazeshp96@gmail.com>
2020-06-25 04:41:12 +0545

    solution of qn 31

---
+++ dataTypes32.py
@@ -0,0 +1,18 @@
+'''
+
+Write a Python script to generate and print a dictionary that contains a 
+number (between 1 and n) in the form (x, x*x). 
+
+Sample Dictionary ( n = 5) : 
+Expected Output : {1: 1, 2: 4, 3: 9, 4: 16, 5: 25} 
+
+'''
+
+
+def genDict(n):
+    result = {i: i*i for i in range(1, n+1)}
+    return result
+
+
+n = int(input("Enter the positive integer:"))
+print(genDict(n))
