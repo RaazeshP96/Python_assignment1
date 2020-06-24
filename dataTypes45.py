97a3c6658d7a505c13c6f6006ce82019020757ea
Raazeshp96 <raazeshp96@gmail.com>
2020-06-25 04:44:31 +0545

    solution of qn 45

---
+++ dataTypes45.py
@@ -0,0 +1,9 @@
+'''
+
+ Write a Python program to find the index of an item of a tuple.
+
+'''
+# sample tuple
+tup = ('a', 'b', 'c', 'd')
+for index, value in enumerate(tup):
+    print(f"{index} -> {value}")
