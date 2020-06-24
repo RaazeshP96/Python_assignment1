3aa4daaa66e55760fd8f0e96bddb829b24ffe235
Raazeshp96 <raazeshp96@gmail.com>
2020-06-24 18:04:34 +0545

    solution of qn12 added

---
+++ dataTypes12.py
@@ -0,0 +1,8 @@
+'''
+Write a Python script that takes input from the user and displays that input
+back in upper and lower cases.
+
+'''
+str1 = input("enter the string:")
+print(f"upper=>{str1.upper()}")
+print(f"lower=>{str1.lower()}")
