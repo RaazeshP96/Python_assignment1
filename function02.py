1bc8b6a7dca5ed520a490b972a54b44888f72bb0
Raazeshp96 <raazeshp96@gmail.com>
2020-06-25 12:31:11 +0545

    solution of function 2

---
+++ function02.py
@@ -0,0 +1,15 @@
+'''
+
+Write a Python function to sum all the numbers in a list. 
+Sample List : [8, 2, 3, 0, 7]
+Expected Output : 20 
+
+'''
+
+
+def sumList(lis):
+    return sum(lis)
+
+
+lis = [8, 2, 3, 0, 7]
+print(sumList(lis))
